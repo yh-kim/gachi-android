5c0f81eea62bc0453779cd0815fa6180e0ea9ae0
Yonghoon Kim <btc_yh@naver.com>
2017-07-24 02:43:45 +0900

    Hide title in bottom navigation item

--- Gachi/app/src/main/kotlin/com/pickth/gachi/view/custom/MyBottomNavigationView.kt
+++ Gachi/app/src/main/kotlin/com/pickth/gachi/view/custom/MyBottomNavigationView.kt
@@ -1,7 +1,6 @@
 package com.pickth.gachi.view.custom
 
 import android.content.Context
-import android.support.design.internal.BottomNavigationItemView
 import android.support.design.internal.BottomNavigationMenuView
 import android.support.design.widget.BottomNavigationView
 import android.util.AttributeSet
@@ -19,26 +18,17 @@ class MyBottomNavigationView(context: Context, attrs: AttributeSet): BottomNavig
 
         val menuView: BottomNavigationMenuView = getChildAt(0) as BottomNavigationMenuView
 
-        val shiftingMode = menuView::class.java.getDeclaredField("mShiftingMode")
-        shiftingMode.isAccessible = true
-        shiftingMode.setBoolean(menuView, false)
-        shiftingMode.isAccessible = false
-
-        if(menuView != null) {
-            for(i in 0..menuView.childCount-1) {
-                val menuItemView = menuView.getChildAt(i) as BottomNavigationItemView
-
-//                val smallText = menuItemView.findViewById(R.id.smallLabel) as TextView
-//                smallText.visibility = View.INVISIBLE
-
-
-//                val icon = menuItemView.getChildAt(0) as AppCompatImageView
-//                val params = icon.layoutParams as LayoutParams
-//                params.gravity = Gravity.CENTER
-
-                menuItemView.setShiftingMode(true)
-                menuItemView.setChecked(menuItemView.itemData.isChecked)
-            }
+        menuView::class.java.getDeclaredField("mShiftingMode").apply { 
+            isAccessible = true
+            setBoolean(menuView, false)
+            isAccessible = false
         }
+
+//        if(menuView != null) {
+//            for(i in 0..menuView.childCount-1) {
+//                val menuItemView = menuView.getChildAt(i) as BottomNavigationItemView
+//
+//            }
+//        }
     }
 }
