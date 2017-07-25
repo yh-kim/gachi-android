3dda7b22fe502741c75e15446c83b88c69f198f4
Yonghoon Kim <btc_yh@naver.com>
2017-07-25 18:09:43 +0900

    Change view

--- Gachi/app/src/main/kotlin/com/pickth/gachi/view/main/fragments/alarm/AlarmFragment.kt
+++ Gachi/app/src/main/kotlin/com/pickth/gachi/view/main/fragments/alarm/AlarmFragment.kt
@@ -7,6 +7,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.pickth.gachi.R
+import com.pickth.gachi.util.MyDividerItemDecoration
 import com.pickth.gachi.view.main.fragments.alarm.adapter.AlarmAdapter
 import kotlinx.android.synthetic.main.fragment_alarm.view.*
 
@@ -30,6 +31,7 @@ class AlarmFragment: Fragment(), AlarmContract.View {
         mAdapter = AlarmAdapter()
         rootView.recycler_alarm.layoutManager = LinearLayoutManager(context, LinearLayoutManager.VERTICAL, false)
         rootView.recycler_alarm.adapter = mAdapter
+        rootView.recycler_alarm.addItemDecoration(MyDividerItemDecoration(context))
 
         // presenter
         mPresenter = AlarmPresenter()
