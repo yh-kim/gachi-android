1e8878693902856b63b2aec0e1177e5470bfafbe
Yonghoon Kim <btc_yh@naver.com>
2017-08-02 22:06:37 +0900

    Change ui of festival page

--- Gachi/app/src/main/kotlin/com/pickth/gachi/view/main/fragments/festival/FestivalFragment.kt
+++ Gachi/app/src/main/kotlin/com/pickth/gachi/view/main/fragments/festival/FestivalFragment.kt
@@ -1,11 +1,15 @@
 package com.pickth.gachi.view.main.fragments.festival
 
 import android.os.Bundle
+import android.support.v7.widget.LinearLayoutManager
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.pickth.gachi.R
 import com.pickth.gachi.base.BaseFragment
+import com.pickth.gachi.view.main.fragments.festival.adapter.Festival
+import com.pickth.gachi.view.main.fragments.festival.adapter.FestivalAdapter
+import kotlinx.android.synthetic.main.fragment_main_festival.view.*
 
 /**
  * Created by yonghoon on 2017-07-20.
@@ -14,6 +18,8 @@ import com.pickth.gachi.base.BaseFragment
 class FestivalFragment: BaseFragment(), FestivalContract.View {
 
     private lateinit var mPresenter: FestivalPresenter
+    private lateinit var mPopularAdapter: FestivalAdapter
+    private lateinit var mImmediateAdapter: FestivalAdapter
 
     companion object {
         private val mInstance = FestivalFragment()
@@ -22,6 +28,26 @@ class FestivalFragment: BaseFragment(), FestivalContract.View {
 
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val rootView = inflater!!.inflate(R.layout.fragment_main_festival, container, false)
+
+
+        mPopularAdapter = FestivalAdapter()
+        mImmediateAdapter = FestivalAdapter()
+
+        rootView.rv_main_festival_popular.run {
+            layoutManager = LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false)
+            adapter = mPopularAdapter
+        }
+
+        rootView.rv_main_festival_immediate.run {
+            layoutManager = LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false)
+            adapter = mImmediateAdapter
+        }
+
+        // test input
+        for(i in 0..5) {
+            mPopularAdapter.addItem(Festival("", "", ""))
+            mImmediateAdapter.addItem(Festival("", "", ""))
+        }
 
         mPresenter = FestivalPresenter()
         mPresenter.attachView(this)
