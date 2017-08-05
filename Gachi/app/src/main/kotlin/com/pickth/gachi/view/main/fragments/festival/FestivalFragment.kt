c02620637a26b7dbf53a043b1c3ff7f4df1edf73
Yonghoon Kim <btc_yh@naver.com>
2017-08-06 00:12:01 +0900

    Change festival ui

--- Gachi/app/src/main/kotlin/com/pickth/gachi/view/main/fragments/festival/FestivalFragment.kt
+++ Gachi/app/src/main/kotlin/com/pickth/gachi/view/main/fragments/festival/FestivalFragment.kt
@@ -6,9 +6,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.pickth.gachi.R
-import com.pickth.gachi.view.main.fragments.TapBaseFragment
 import com.pickth.gachi.util.MyDividerItemDecoration
 import com.pickth.gachi.view.festival.FestivalDetailActivity
+import com.pickth.gachi.view.main.fragments.TapBaseFragment
 import com.pickth.gachi.view.main.fragments.festival.adapter.Festival
 import com.pickth.gachi.view.main.fragments.festival.adapter.FestivalAdapter
 import kotlinx.android.synthetic.main.fragment_main_festival.view.*
@@ -40,13 +40,13 @@ class FestivalFragment : TapBaseFragment(), FestivalContract.View {
         rootView.rv_main_festival_popular.run {
             layoutManager = LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false)
             adapter = mPopularAdapter
-            addItemDecoration(MyDividerItemDecoration(context, LinearLayoutManager.HORIZONTAL, 16, false))
+            addItemDecoration(MyDividerItemDecoration(context, LinearLayoutManager.HORIZONTAL, 10, false))
         }
 
         rootView.rv_main_festival_immediate.run {
             layoutManager = LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false)
             adapter = mImmediateAdapter
-            addItemDecoration(MyDividerItemDecoration(context, LinearLayoutManager.HORIZONTAL, 16, false))
+            addItemDecoration(MyDividerItemDecoration(context, LinearLayoutManager.HORIZONTAL, 10, false))
         }
 
         // test input
