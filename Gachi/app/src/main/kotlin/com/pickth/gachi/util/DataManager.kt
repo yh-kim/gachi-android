67129b447a2734caf7e24d1ec1a2c2212d9b1e2a
Yonghoon Kim <btc_yh@naver.com>
2017-08-08 00:51:05 +0900

    Change singleton code

--- Gachi/app/src/main/kotlin/com/pickth/gachi/util/DataManager.kt
+++ Gachi/app/src/main/kotlin/com/pickth/gachi/util/DataManager.kt
@@ -6,18 +6,16 @@ import android.content.Context
  * Created by yonghoon on 2017-07-12.
  * Mail   : yonghoon.kim@pickth.com
  */
-class DataManager {
-    companion object {
-        fun setAppPreferences(context: Context, key: String, value: String) {
-            val pref = context.getSharedPreferences("gachi", 0)
-            val prefEditor = pref.edit()
-            prefEditor.putString(key, value)
-                    .commit()
-        }
+object DataManager {
+    fun setAppPreferences(context: Context, key: String, value: String) {
+        val pref = context.getSharedPreferences("gachi", 0)
+        val prefEditor = pref.edit()
+        prefEditor.putString(key, value)
+                .commit()
+    }
 
-        fun getAppPreferences(context: Context, key: String): String {
-            val prefEditor = context.getSharedPreferences("gachi", 0)
-            return prefEditor.getString(key, "")
-        }
+    fun getAppPreferences(context: Context, key: String): String {
+        val prefEditor = context.getSharedPreferences("gachi", 0)
+        return prefEditor.getString(key, "")
     }
 }
--- Gachi/app/src/main/kotlin/com/pickth/gachi/util/FontManager.kt
+++ Gachi/app/src/main/kotlin/com/pickth/gachi/util/FontManager.kt
@@ -10,23 +10,21 @@ import android.widget.TextView
  * Created by yonghoon on 2017-07-12.
  * Mail   : yonghoon.kim@pickth.com
  */
-class FontManager {
-    companion object {
-        fun setGlobalFont(context: Context, view: View) {
-            if (view is ViewGroup)
-                for (i in 0..view.childCount - 1) {
-                    val v = view.getChildAt(i)
-                    setViewFont(context, v)
+object FontManager {
+    fun setGlobalFont(context: Context, view: View) {
+        if (view is ViewGroup)
+            for (i in 0..view.childCount - 1) {
+                val v = view.getChildAt(i)
+                setViewFont(context, v)
 
-                    setGlobalFont(context, v)
-                }
-        }
-
-        fun setViewFont(context: Context, view: View) {
-            if(view is TextView) {
-                view.typeface = Typeface.createFromFile("/system/fonts/DroidSans.ttf")
-//                Dlog.v("Set font. id : ${view.id}")
+                setGlobalFont(context, v)
             }
+    }
+
+    fun setViewFont(context: Context, view: View) {
+        if(view is TextView) {
+            view.typeface = Typeface.createFromFile("/system/fonts/DroidSans.ttf")
+//                Dlog.v("Set font. id : ${view.id}")
         }
     }
 }
