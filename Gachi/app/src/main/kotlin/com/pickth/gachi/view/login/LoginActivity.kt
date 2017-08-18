306c223fecde2dfd541737379db1da04244cc436
Yonghoon Kim <btc_yh@naver.com>
2017-08-19 02:19:39 +0900

    Add facebook login with firebase

--- Gachi/app/src/main/kotlin/com/pickth/gachi/view/login/LoginActivity.kt
+++ Gachi/app/src/main/kotlin/com/pickth/gachi/view/login/LoginActivity.kt
@@ -1,12 +1,23 @@
 package com.pickth.gachi.view.login
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import com.facebook.AccessToken
+import com.facebook.CallbackManager
+import com.facebook.FacebookCallback
+import com.facebook.FacebookException
+import com.facebook.login.LoginResult
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.auth.FacebookAuthProvider
+import com.google.firebase.auth.FirebaseAuth
 import com.pickth.gachi.R
 import com.pickth.gachi.base.BaseActivity
 import com.pickth.gachi.view.main.MainActivity
 import com.pickth.gachi.view.signup.SignupActivity
 import kotlinx.android.synthetic.main.activity_login.*
 import org.jetbrains.anko.startActivity
+import org.jetbrains.anko.toast
 
 /**
  * Created by yonghoon on 2017-07-09.
@@ -14,13 +25,59 @@ import org.jetbrains.anko.startActivity
  */
 class LoginActivity : BaseActivity() {
 
+    private lateinit var mAuth: FirebaseAuth
+    private lateinit var mAuthListener: FirebaseAuth.AuthStateListener
+
+    private lateinit var mCallbackManager: CallbackManager
+
+    companion object {
+        val TAG = "GACHI_LoginActivity"
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
+        mAuth = FirebaseAuth.getInstance()
+        mAuthListener = FirebaseAuth.AuthStateListener {
+            var user = it.currentUser
+            if(user != null) {
+                Log.d(TAG, "onAuthStateChanged:signed_in: ${user.uid}")
+                startActivity<MainActivity>()
+                finish()
+            } else {
+                Log.d(TAG, "onAuthStateChanged:signed_out")
+            }
+        }
+
+
+        mCallbackManager = CallbackManager.Factory.create()
+
+        lb_facebook_login.setReadPermissions("email", "public_profile")
+        lb_facebook_login.registerCallback(mCallbackManager, object: FacebookCallback<LoginResult> {
+            override fun onCancel() {
+                Log.d(TAG, "facebook:onCancel")
+            }
+
+            override fun onError(error: FacebookException?) {
+                error?.printStackTrace()
+                Log.d(TAG, "facebook:onError", error)
+            }
+
+            override fun onSuccess(result: LoginResult) {
+                Log.d(TAG, "facebook:onSuccess: $result")
+                handleFacebookAccessToken(result.accessToken)
+            }
+
+        })
+
+
         fl_login_with_email.setOnClickListener {
         }
 
+        fl_login_with_facebook.setOnClickListener {
+            lb_facebook_login.performClick()
+        }
 
         tv_signup.setOnClickListener {
             startActivity<SignupActivity>()
@@ -34,6 +91,21 @@ class LoginActivity : BaseActivity() {
 
     }
 
+    fun handleFacebookAccessToken(token: AccessToken) {
+        Log.d(TAG, "handleFacebookAccessToken: $token")
+
+        var credential = FacebookAuthProvider.getCredential(token.token)
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, OnCompleteListener {
+                    Log.d(TAG, "signInWithCredential:onComplete: ${it.isSuccessful}")
+
+                    if(!it.isSuccessful) {
+                        Log.d(TAG, "signInWithCredential: ${it.exception}")
+                        toast("Authentication failed.")
+                    }
+                })
+    }
+
     override fun onDestroy() {
         super.onDestroy();
     }
@@ -43,6 +115,19 @@ class LoginActivity : BaseActivity() {
         finish()
     }
 
+    override fun onStart() {
+        super.onStart()
+        mAuth.addAuthStateListener(mAuthListener)
+    }
 
+    override fun onStop() {
+        super.onStop()
+        if(mAuthListener != null) mAuth.removeAuthStateListener(mAuthListener)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        mCallbackManager.onActivityResult(requestCode, resultCode, data)
+    }
 }
 
--- Gachi/app/src/main/kotlin/com/pickth/gachi/view/splash/SplashActivity.kt
+++ Gachi/app/src/main/kotlin/com/pickth/gachi/view/splash/SplashActivity.kt
@@ -2,6 +2,8 @@ package com.pickth.gachi.view.splash
 
 import android.os.Bundle
 import android.os.Handler
+import android.util.Log
+import com.google.firebase.auth.FirebaseAuth
 import com.pickth.gachi.R
 import com.pickth.gachi.base.BaseActivity
 import com.pickth.gachi.view.login.LoginActivity
@@ -14,18 +16,41 @@ import org.jetbrains.anko.startActivity
  */
 
 class SplashActivity : BaseActivity() {
+    private lateinit var mAuth: FirebaseAuth
+    private lateinit var mAuthListener: FirebaseAuth.AuthStateListener
+    companion object {
+        val TAG = "GACHI_SplashActivity"
+    }
+
+    override fun onStart() {
+        super.onStart()
+        mAuth.addAuthStateListener(mAuthListener)
+    }
+
+    override fun onStop() {
+        super.onStop()
+        if(mAuthListener != null) mAuth.removeAuthStateListener(mAuthListener)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
 
-        // 0.8 sec
-        Handler().postDelayed(mTimer, 8*100)
-    }
+        mAuth = FirebaseAuth.getInstance()
+        mAuthListener = FirebaseAuth.AuthStateListener {
+            var user = it.currentUser
+            if(user != null) {
+                Log.d(TAG, "onAuthStateChanged:signed_in: ${user.uid} ${user.photoUrl}")
 
-    private fun getUser() {
-        startActivity<MainActivity>()
-        finish()
+                // 0.8 sec
+                Handler().postDelayed(mTimer, 8*100)
+            } else {
+                Log.d(TAG, "onAuthStateChanged:signed_out")
+                startActivity<LoginActivity>()
+                finish()
+            }
+        }
+
     }
 
     override fun onBackPressed() {
@@ -33,8 +58,7 @@ class SplashActivity : BaseActivity() {
     }
 
     private val mTimer = Runnable {
-//        getUser()
-        startActivity<LoginActivity>()
+        startActivity<MainActivity>()
         finish()
     }
 }
